06df54b3ae532ef6384f9e556467fc1a6fa19c3c
Michael Hinte <michaelhinte@michaels-mbp-2.attlocal.net>
2019-09-12 14:24:02 -0500

    creates hero model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Hero(models.Model):
+    name = models.CharField(max_length=100)
+    hero_class = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    level = models.IntegerField()
