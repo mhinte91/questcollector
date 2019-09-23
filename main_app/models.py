725a2916591cf2d2117e8922d3f4aa574461ec95
Michael Hinte <michaelhinte@michaels-mbp-2.attlocal.net>
2019-09-23 07:44:43 -0500

    pre-many-to-many

--- main_app/models.py
+++ main_app/models.py
@@ -9,6 +9,25 @@ TYPE = (
     ('M', 'Magic Item')
 )
 
+DIFFICULTY = (
+    ('E', 'Easy'),
+    ('H', 'Heroic'),
+    ('L', 'Legendary')
+)
+
+class Quest(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField(max_length=2000)
+    difficulty = models.CharField(
+        max_length=1,
+        choices=DIFFICULTY,
+        default=DIFFICULTY[1][0]
+    )
+    def __str__(self):
+        return self.name
+    def get_absolute_url(self):
+        return reverse('quests_detail', kwargs={'pk': self.id})
+
 class Hero(models.Model):
     name = models.CharField(max_length=100)
     hero_class = models.CharField(max_length=100)
